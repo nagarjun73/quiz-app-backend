a6eca83e6b79cd9c42b97eed84f0e3498df94a3c
Nagarjun <nagarjun7397@gmail.com>
2023-10-22 22:00:11 +0530

    express server setup

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+
+const port = process.env.PORT
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+
+app.listen(port, () => {
+  console.log('Server running on port', port)
+})
